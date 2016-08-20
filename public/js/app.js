b4f3025ab7efa993f009c3b22ba6981a796c8f4a
dpcc <drewpcchannel@gmail.com>
2016-08-19 17:00:48 -1000

    added custom charts to links

--- public/js/app.js
+++ public/js/app.js
@@ -10,16 +10,25 @@ var counterApp = angular.module('counterApp', []);
 
 counterApp.controller('divHideController', ['$scope', function($scope) {
     $scope.count = 0;
-    $scope.loadBigFunction = function () {
+    $scope.loadBigFunction = function (chartSelected) {
       $scope.showHide = !$scope.showHide;
+      $scope[chartSelected] = !$scope[chartSelected];
     };
+    $scope.returnHome = function () {
+      $scope.showHide = !$scope.showHide;
+      $scope.chartDegreeBigShow = false;
+      $scope.chartMajorBigShow = false;
+      $scope.chartHasDebtBigShow = false;
+    };
     $scope.showHide = false;
     $scope.pageNumber = 1;
     $scope.gotoNextPage = function() {
       $scope.pageNumber++;
-      console.log($scope.pageNumber);
     };
     $scope.testFrog = true;
+    $scope.chartDegreeBigShow = false;
+    $scope.chartMajorBigShow = false;
+    $scope.chartHasDebtBigShow = false;
 }]);
 
 // app , controller, scope, directives
