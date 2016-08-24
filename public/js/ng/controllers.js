64d0503c4135b4318b9d96cc7994cabad86b6ea6
Matt Olsen <olsen_matthew@yahoo.com>
2016-08-23 15:44:26 -1000

    trying to modularize ng code

--- public/js/ng/controllers.js
+++ public/js/ng/controllers.js
@@ -1,13 +1,5 @@
 var counterApp = angular.module('counterApp', []);
 
-/*counterApp.factory('arrowHider', ['$arrowHiderBoolean', function($arrowHiderBoolean) {
-  var showArrow = true;
-  $arrowHiderBoolean.arrowHider = function () {
-    console.log('inside the factory' + showArrow);
-    showArrow = !showArrow;
-  };
-}]);*/
-
 counterApp.controller('divHideController', ['$scope', function($scope) {
     $scope.count = 0;
     $scope.loadBigFunction = function (chartSelected) {
@@ -105,5 +97,3 @@ counterApp.controller('divHideController', ['$scope', function($scope) {
     };
     $scope.testFrog = true;
 }]);
-
-// app , controller, scope, directives
